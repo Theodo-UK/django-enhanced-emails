3ee8699e1aff7e2cb5e324743c42c35171843924
Nathan Gaberel <nathang@theodo.co.uk>
2018-04-16 17:36:42 +0100

    Include license + python files.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='django-enhanced-emails',
-    version='0.0.4',
+    version='0.0.5',
     description='Enhanced email classes for Django',
     long_description=readme(),
     long_description_content_type='text/markdown',
