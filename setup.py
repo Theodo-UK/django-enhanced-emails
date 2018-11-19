ec3a0d3d255735551c4040f6eaa1215f165cefc8
Nathan Gaberel <nathan@gnab.fr>
2018-11-19 12:42:22 +0000

    v0.0.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="django-enhanced-emails",
-    version="0.0.6",
+    version="0.0.7",
     description="Enhanced email classes for Django",
     long_description=readme(),
     long_description_content_type="text/markdown",
